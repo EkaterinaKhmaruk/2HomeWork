49069b2967cc53794ea0abac7d86904ad7ef4f98
Ekaterina Khmaruk <ekaterinabond43@gmail.com>
2022-11-16 15:41:04 +0500

    Added falders

---
+++ 1/1.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+bool correct = false;
+
+while(!correct)
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number / 100 == 0 || number / 100 > 9)
+    {
+          Console.WriteLine("Некорректное число, введите число из 3 цифр! ");
+    }
+    else
+    {
+        correct = true;
+        int secondtDigit1 = number / 10;
+        int secondtDigit2 = secondtDigit1 % 10;
+        int firstDigit = number / 100;
+        int lastDigit = number % 10;
+
+
+        Console.WriteLine("Вторая цифра этого числа: "+ secondtDigit2);
+    }  
+}
+
+
+
+
+
+
+
---
+++ 3/3.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+bool correct = false;
+
+while(!correct)
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number < 1 || number > 7)
+    {
+          Console.WriteLine("Некорректное число. ");
+    }
+    else if (number == 6 || number == 7) 
+    {
+        Console.WriteLine("Выходной!");
+        break;
+    }
+    else
+    {
+        correct = true;
+        Console.WriteLine("Будние");
+    }  
+}
